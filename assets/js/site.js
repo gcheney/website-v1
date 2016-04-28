00dc13bcb240f17760632198e6ade07807f88e4e
Glendon Cheney <glendon.cheney@gmail.com>
2016-04-28 12:43:33 -0500

    Add minified JS and CSS files

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,9 +1,3 @@
-/*
-	Read Only by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	skel.breakpoints({
@@ -22,91 +16,91 @@
 			$wrapper = $('#wrapper');
 
 		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+        $('form').placeholder();
 
 		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+        skel.on('+medium -medium', function() {
+            $.prioritize(
+                '.important\\28 medium\\29',
+                skel.breakpoint('medium').active
+            );
+        });
 
 		// Header.
-			var ids = [];
+        var ids = [];
 
-			// Set up nav items.
-				$nav_a
-					.scrolly({ offset: 44 })
-					.on('click', function(event) {
+        // Set up nav items.
+        $nav_a
+            .scrolly({ offset: 44 })
+            .on('click', function(event) {
 
-						var $this = $(this),
-							href = $this.attr('href');
+                var $this = $(this),
+                    href = $this.attr('href');
 
-						// Not an internal link? Bail.
-							if (href.charAt(0) != '#')
-								return;
+                // Not an internal link? Bail.
+                    if (href.charAt(0) != '#')
+                        return;
 
-						// Prevent default behavior.
-							event.preventDefault();
+                // Prevent default behavior.
+                    event.preventDefault();
 
-						// Remove active class from all links and mark them as locked (so scrollzer leaves them alone).
-							$nav_a
-								.removeClass('active')
-								.addClass('scrollzer-locked');
+                // Remove active class from all links and mark them as locked (so scrollzer leaves them alone).
+                    $nav_a
+                        .removeClass('active')
+                        .addClass('scrollzer-locked');
 
-						// Set active class on this link.
-							$this.addClass('active');
+                // Set active class on this link.
+                    $this.addClass('active');
 
-					})
-					.each(function() {
+            })
+            .each(function() {
 
-						var $this = $(this),
-							href = $this.attr('href'),
-							id;
+                var $this = $(this),
+                    href = $this.attr('href'),
+                    id;
 
-						// Not an internal link? Bail.
-							if (href.charAt(0) != '#')
-								return;
+                // Not an internal link? Bail.
+                    if (href.charAt(0) != '#')
+                        return;
 
-						// Add to scrollzer ID list.
-							id = href.substring(1);
-							$this.attr('id', id + '-link');
-							ids.push(id);
+                // Add to scrollzer ID list.
+                    id = href.substring(1);
+                    $this.attr('id', id + '-link');
+                    ids.push(id);
 
-					});
+            });
 
-			// Initialize scrollzer.
-				$.scrollzer(ids, { pad: 300, lastHack: true });
+        // Initialize scrollzer.
+        $.scrollzer(ids, { pad: 300, lastHack: true });
 
 		// Off-Canvas Navigation.
 
-			// Title Bar.
-				$(
-					'<div id="titleBar">' +
-						'<a href="#header" class="toggle"></a>' +
-						'<span class="title">' + $('#logo').html() + '</span>' +
-					'</div>'
-				)
-					.appendTo($body);
+        // Title Bar.
+        $(
+            '<div id="titleBar">' +
+                '<a href="#header" class="toggle"></a>' +
+                '<span class="title">' + $('#logo').html() + '</span>' +
+            '</div>'
+        )
+            .appendTo($body);
 
-			// Header.
-				$('#header')
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'right',
-						target: $body,
-						visibleClass: 'header-visible'
-					});
+        // Header.
+        $('#header')
+            .panel({
+                delay: 500,
+                hideOnClick: true,
+                hideOnSwipe: true,
+                resetScroll: true,
+                resetForms: true,
+                side: 'right',
+                target: $body,
+                visibleClass: 'header-visible'
+            });
 
-			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#titleBar, #header, #wrapper')
-						.css('transition', 'none');
+        // Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+        if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+            $('#titleBar, #header, #wrapper')
+                .css('transition', 'none');
 
 	});
 
